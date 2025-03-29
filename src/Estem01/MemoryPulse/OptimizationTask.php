b63b04d8ebb6c3094d1032e5ddde84de4ddb99a0
Estem0 <101932306+Estem01@users.noreply.github.com>
2025-03-29 01:24:03 -0300

    Update OptimizationTask.php

--- src/Estem01/MemoryPulse/OptimizationTask.php
+++ src/Estem01/MemoryPulse/OptimizationTask.php
@@ -15,10 +15,10 @@ class OptimizationTask extends Task {
     public function onRun(): void {
         $this->pulseEvent->optimizeMemory();
 
-        if ($this->pulseEvent->main->getPluginConfig()->get("logger", false)) {
+        if ($this->pulseEvent->getMain()->getPluginConfig()->get("logger", false)) {
             $memoryUsage = memory_get_usage(true) / 1024 / 1024;
             $peakMemory = memory_get_peak_usage(true) / 1024 / 1024;
-            $this->pulseEvent->main->getLogger()->info(sprintf(
+            $this->pulseEvent->getMain()->getLogger()->info(sprintf(
                 "Memory stats - Usage: %.2f MB | Peak: %.2f MB | Packets: %d | Avg Usage: %.2f MB | Avg Packet Rate: %.2f p/s | Interval: %ds | Impact: %.2f",
                 $memoryUsage,
                 $peakMemory,
